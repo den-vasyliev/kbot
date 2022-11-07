46146ea9f2994bb9c8eb75df58376d2fd168d367
Denys Vasyliev <den.vasyliev@gmail.com>
2022-11-07 01:56:33 +0000

    add start by default

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -86,12 +86,13 @@ to quickly create a Cobra application.`,
 					pin.Input()
 					trafficSignal[payload]["on"] = 0
 				}
-				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
 
-				//err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
+				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
 
 			default:
-				err = m.Send("Usage: /s red|amber|green")
+				//err = m.Send("Usage: /s red|amber|green")
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
 			}
 
 			return err
