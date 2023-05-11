2b7d162c36624c299dcf496189047ab37c44427d
Denys Vasyliev <den.vasyliev@gmail.com>
2023-05-11 15:02:30 +0100

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,1 +1,19 @@
+pipeline {
+    agent any
+    parameters {
 
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
+
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+
+                echo "Build for arch: ${params.ARCH}"
+
+            }
+        }
+    }
+}
