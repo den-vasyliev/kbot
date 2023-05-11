f0f6c93ed958e643ce63aa125e43cc4a042564a9
Denys Vasyliev <den.vasyliev@gmail.com>
2023-05-11 15:09:56 +0100

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -3,7 +3,6 @@ pipeline {
     parameters {
 
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'all'], description: 'Pick ARCH')
 
     }
     stages {
