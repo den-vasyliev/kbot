390f1c29e867d24f185bf5c6db2e95a916324134
Denys Vasyliev <den.vasyliev@gmail.com>
2025-04-08 22:00:39 +0100

    add lic (#7)

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,18 +1,26 @@
 pipeline {
     agent any
+    
     parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
-    }
-    stages {
-        stage('Example') {
-            steps {
-                echo "Build for platform ${params.OS}"
-
-                echo "Build for arch: ${params.ARCH}"
-
-            }
-        }
+        choice(
+            name: 'OS',
+            choices: ['linux', 'darwin', 'windows'],
+            description: 'Target operating system'
+        )
+        choice(
+            name: 'ARCH',
+            choices: ['amd64', 'arm64'],
+            description: 'Target architecture'
+        )
+        booleanParam(
+            name: 'SKIP_TESTS',
+            defaultValue: false,
+            description: 'Skip running tests'
+        )
+        booleanParam(
+            name: 'SKIP_LINT',
+            defaultValue: false,
+            description: 'Skip running linter'
+        )
     }
 }
